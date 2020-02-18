d4a02ea90da86ed839997403af3b288924dab876
James Stevenson <jpatstevenson@gmail.com>
2020-02-18 12:51:53 -0800

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,72 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+//constructor for web3
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+const INITIAL_STRING = 'Hi there!';
+
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy
+    // the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        //tells web3 to create a contract (call contructor of contract 'Inbox')
+        .deploy({ 
+            data: bytecode, 
+            arguments: ['Hi there!'] 
+        })
+        //sends communication from web3 to the network
+        .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        //does inbox address exist?
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
+    });
+});
+
+/*
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+}); 
+*/
